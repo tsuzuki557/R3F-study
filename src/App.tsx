8e02b0444584c252a0861299754e12675b3e2fd1
tsuzuki <goodin557@gmail.com>
2023-01-26 11:04:13 +0900

    first step

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Basic } from "./components/Basic";
+
+export function App() {
+  return (
+    <div>
+      <Basic />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
+import { App } from "./App";
+
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hellow</h1>
+    <App />
   </React.StrictMode>
 );
