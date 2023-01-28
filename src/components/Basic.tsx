95525020e781a99daef5bb99816ee8e70de6c248
tsuzuki <goodin557@gmail.com>
2023-01-28 16:36:56 +0900

    drei試す

--- src/components/Basic.tsx
+++ src/components/Basic.tsx
@@ -1,5 +1,35 @@
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import * as THREE from "three";
+import {
+  Canvas,
+  useFrame,
+  ThreeElements,
+  extend,
+  useThree,
+  ReactThreeFiber,
+} from "@react-three/fiber";
+
+import {
+  OrbitControls,
+  TransformControls,
+  Html,
+  PivotControls,
+} from "@react-three/drei";
+
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+//extend({ OrbitControls: OrbitControls });
+
+// ref. https://github.com/pmndrs/react-three-fiber/issues/130
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       orbitControls: ReactThreeFiber.Object3DNode<
+//         OrbitControls,
+//         typeof OrbitControls
+//       >;
+//     }
+//   }
+// }
 
 function Box(props: ThreeElements["mesh"]) {
   const mesh = useRef<THREE.Mesh>(null!);
@@ -28,12 +58,49 @@ function Box(props: ThreeElements["mesh"]) {
   );
 }
 
+function Sample(props: ThreeElements["mesh"]) {
+  const { camera, gl } = useThree();
+
+  const boxRef = useRef<THREE.Mesh>(null!);
+
+  return (
+    <>
+      {/* <orbitControls args={[camera, gl.domElement]} /> */}
+      <OrbitControls enableDamping={true} makeDefault />
+
+      <group>
+        {/* <TransformControls object={boxRef} /> */}
+
+        <Html scale={0.2} castShadow receiveShadow occlude="blending" transform>
+          <h1>タイトル</h1>
+        </Html>
+
+        <PivotControls>
+          <mesh ref={boxRef} position={[0, 0, -2]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshBasicMaterial color={"rgb(255, 0, 0)"} wireframe={false} />
+            {/* <Html>
+              <h1>タイトル</h1>
+            </Html> */}
+          </mesh>
+        </PivotControls>
+
+        {/* <mesh>
+      <sphereGeometry />
+      <meshBasicMaterial color="orange" />
+    </mesh> */}
+      </group>
+    </>
+  );
+}
+
 export function Basic() {
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10, 10, 10]} /> */}
+      {/* <Box position={[-1.2, 0, 0]} /> */}
+      <Sample />
     </Canvas>
   );
 }
